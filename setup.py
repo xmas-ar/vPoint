7f36debb3b2467f972687c69f991e3397a0b3255
xmas-ar <christianmas2011@gmail.com>
2025-04-24 15:53:32 +0200

    Release package 0.3.0

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+# Ensure we can read requirements.txt
+if os.path.exists('requirements.txt'):
+    with open('requirements.txt') as f:
+        requirements = [line.strip() for line in f.readlines() if line.strip()]
+else:
+    requirements = ['prompt_toolkit>=3.0.0', 'pyyaml>=6.0']
+
+setup(
+    name="vmark-node",
+    version="0.3.0",
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'vmark-node=main:start_cli',
+        ],
+    },
+    author="Pathgate",
+    description="An Ethernet software-based open source demarcation NID",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/xmas-ar/vMark-node",
+    license="GPL-3.0-only",  # Use SPDX identifier instead of classifier
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # Remove license classifier as recommended
+        "Operating System :: OS Independent",
+    ],
+)
